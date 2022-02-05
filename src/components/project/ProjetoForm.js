f2f397b54eab8481dc7abb00187fc1e237c25a44
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-02-05 00:48:51 -0300

    Criando o backend da aplicação

--- src/components/project/ProjetoForm.js
+++ src/components/project/ProjetoForm.js
@@ -1,9 +1,27 @@
+import { useEffect, useState} from 'react'
+
 import Input from '../Form/Input'
 import Select from '../Form/Select'
 import SubmitButton from '../Form/SubmitButton'
 import styles from './ProjetoForm.module.css'
 
 function ProjetoForm({ btnText }) {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setCategories(data)
+    })
+    .catch((err) => console.log(err))
+  }, [])
+
   return (
     <form className={styles.form}>
       <Input
@@ -21,6 +39,7 @@ function ProjetoForm({ btnText }) {
       <Select
         name="category_id"
         text="Selecione a categoria:"
+        options={categories}
       />
       <SubmitButton text={btnText}/>
     </form>
