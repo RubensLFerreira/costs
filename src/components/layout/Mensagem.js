8aa6d1b2661ba0500422a4213d639efdbab50c29
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-02-12 15:37:10 -0300

    Finalizando o sistemas de mensagens
    O sistema de mensagem é dinâmico, será usado quando o projeto for criado com sucesso ou quando surgir algum erro.

--- src/components/layout/Mensagem.js
+++ src/components/layout/Mensagem.js
@@ -1,8 +1,28 @@
+import { useState, useEffect } from 'react'
 import styles from './Mensagem.module.css';
 
 function Mensagem({ type, msg }) {
+  const [visible, setVisible] = useState(false)
+
+  useEffect(() =>{
+    if(!msg) {
+      setVisible(false)
+      return
+    }
+    setVisible(true)
+
+    const timer = setTimeout(() => {
+      setVisible(false)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [msg])
   return(
-   <p><div className={`${styles.Mensagem} ${styles[type]}`}>{msg}</div></p>
+   <>
+    {visible && (
+      <p><div className={`${styles.mensagem} ${styles[type]}`}>{msg}</div></p>
+    )}
+   </>
   )
 }
 
--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,10 +1,18 @@
+import { useLocation } from 'react-router-dom'
+
 import Mensagem from "../components/layout/Mensagem"
 
 function Projetos() {
+  const location = useLocation();
+  let message = ''
+  if(location.state){
+    message = location.state.message
+  }
+
   return (
     <div>
       <h1>Meus projeto</h1>
-      <Mensagem msg="Alguma mensagem" />
+      {message && <Mensagem type="sucess" msg={message} />}
     </div>
   )
 }
