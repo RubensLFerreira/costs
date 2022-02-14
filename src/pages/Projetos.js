c11c5322a01da3bf5d1eb03d93703e05c5c2e52e
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-02-14 12:39:31 -0300

    Criando dashboard em Projetos
    Criando a área de projetos e estilando o componente.

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,6 +1,10 @@
 import { useLocation } from 'react-router-dom'
 
 import Mensagem from "../components/layout/Mensagem"
+import Container from '../components/layout/Container'
+import LinkButton from '../components/layout/LinkButton'
+
+import styles from './Projetos.module.css'
 
 function Projetos() {
   const location = useLocation();
@@ -10,9 +14,15 @@ function Projetos() {
   }
 
   return (
-    <div>
-      <h1>Meus projeto</h1>
+    <div className={styles.project_container}>
+      <div className={styles.tittle_container}>
+        <h1>Meus projeto</h1>
+        <LinkButton to="/novoprojeto" text="Criar projeto" />
+      </div>
       {message && <Mensagem type="sucess" msg={message} />}
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   )
 }
