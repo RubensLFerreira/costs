31ef365f810e52fea7332a393a043c9a929ce3ce
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-02-17 10:39:53 -0300

    Projeto Card
    Criando Projeto Card que irá receber os projetos criados contendo todos os detalhes do projeto vindo do db.json

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,17 +1,35 @@
 import { useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 import Mensagem from "../components/layout/Mensagem"
 import Container from '../components/layout/Container'
 import LinkButton from '../components/layout/LinkButton'
+import ProjectCard from '../components/project/ProjectCard'
 
 import styles from './Projetos.module.css'
 
 function Projetos() {
+  const [projects, setProjects] = useState([])
+
   const location = useLocation();
   let message = ''
   if(location.state){
     message = location.state.message
   }
+
+  useEffect(() => {
+    fetch('http://localhost:5000/projetos', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setProjects(data)
+    })
+    .catch((err) => console.log(err))
+  }, [])
 
   return (
     <div className={styles.project_container}>
@@ -21,7 +39,15 @@ function Projetos() {
       </div>
       {message && <Mensagem type="sucess" msg={message} />}
       <Container customClass="start">
-        <p>Projetos...</p>
+        {projects.length > 0 &&
+          projects.map((project) => 
+          <ProjectCard
+            id={project.id}
+            name={project.name}
+            budget={project.budget}
+            category={project.category}
+            key={project.key}
+          />)}
       </Container>
     </div>
   )
